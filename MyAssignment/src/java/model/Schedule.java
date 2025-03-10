37fa13d02dfec62cf3df55acfc575ee0f10e9e03
quangdv <quangdvhe180364@gmail.com>
2025-03-10 17:38:04 +0700

    create model Schedule

---
+++ MyAssignment/src/java/model/Schedule.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author admin
+ */
+public class Schedule {
+    private int id;
+    private int employeeId;
+    private Date date;
+    private String status;
+
+    public Schedule() {
+    }
+
+    public Schedule(int id, int employeeId, Date date, String status) {
+        this.id = id;
+        this.employeeId = employeeId;
+        this.date = date;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    
+}
